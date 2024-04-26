3f0e2eb699a719a7e72433b90c3c25c3e9814233
spearboy <95023009+spearboy@users.noreply.github.com>
2024-04-26 14:22:36 +0900

    Update chartMelon.py

--- chartMelon.py
+++ chartMelon.py
@@ -4,13 +4,13 @@ import pandas as pd
 import datetime
 
 current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-res = req.get("https://music.bugs.co.kr/chart")
+res = req.get("https://www.melon.com/chart/index.htm")
 soup = bs(res.text, "lxml")
 
 # 데이터 선택
-ranking = soup.select(".ranking > strong")
-title = soup.select(".title > a")
-artist = soup.select(".artist > a:nth-child(1)")
+ranking = soup.select("tbody .rank")
+title = soup.select(".wrap_song_info > .rank01 > span > a")
+artist = soup.select(".wrap_song_info > .rank02 > span > a:nth-child(1)")
 
 # 데이터 저장
 rankingList = [r.text.strip() for r in ranking]
@@ -25,5 +25,5 @@ chart_df = pd.DataFrame({
 })
 
 # JSON 파일로 저장
-file_name = f"bugsChart100_{current_date}.json"
+file_name = f"melonChart100_{current_date}.json"
 chart_df.to_json(file_name, force_ascii=False, orient="records")
